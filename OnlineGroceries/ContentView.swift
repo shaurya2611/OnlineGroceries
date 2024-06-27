8c88e882e8cd49987c99e36ede46eb0bb955fe6f
Shaurya Singh <shaurya2611@gmail.com>
2024-06-27 22:51:37 +0530

    Initial Commit

---
+++ OnlineGroceries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineGroceries
+//
+//  Created by Shaurya Singh on 27/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesApp.swift
+//  OnlineGroceries
+//
+//  Created by Shaurya Singh on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
