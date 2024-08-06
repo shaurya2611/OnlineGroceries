1c1205deee71d70b0b722c74d77e1faba21155cf
Shaurya Singh <shaurya2611@gmail.com>
2024-08-06 10:52:10 +0530

    sign in view code edit

---
+++ OnlineGroceries/View/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  OnlineGroceries
+//
+//  Created by Shaurya Singh on 04/08/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
